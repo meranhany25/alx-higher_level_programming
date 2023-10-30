b123b6b6191733b203c6971373b4b5bff266f4bc
meranhany25 <meranhany25@gmail.com>
2023-10-30 15:46:53 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,32 @@
-#include "lists.h"
 #include <stdlib.h>
-
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - check list
+ * @list: list
+ * Return: 1 or  0.
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *prev = NULL;
-	listint_t *curr = NULL;
+	listint_t *iter1;
+	listint_t *iter2;
 
+	iter1 = list;
+	iter2 = list;
 	if (list == NULL)
 	{
 		return (0);
 	}
-
-
-	prev = list;
-	curr = list;
-
-	while (curr != NULL && prev != NULL)
+	iter2 = iter2->next;
+	while (iter2 != NULL && iter2->next != NULL && iter1 != NULL)
 	{
-		curr = curr->next;
-		if (curr == prev)
+		if (iter1 == iter2)
 		{
 			return (1);
 		}
-		prev = prev->next;
-		curr = curr->next;
-
+		iter1 = iter1->next;
+		iter2 = iter2->next->next;
 	}
-
 	return (0);
 }

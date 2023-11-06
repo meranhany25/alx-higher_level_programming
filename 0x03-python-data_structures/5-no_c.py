0a98c75089f80d2807d06359346bae5985fa0f48
meranhany25 <meranhany25@gmail.com>
2023-11-06 02:18:28 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    string_without_c = ""
+    string_without_c = []
 
     for c in my_string:
         if c != 'c' and c != 'C':
-            string_without_c += c
+            string_without_c.append(c)
 
-    return(string_without_c)
+    return ''.join(string_without_c)

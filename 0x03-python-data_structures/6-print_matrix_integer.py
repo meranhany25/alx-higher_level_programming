98914e35b723bc64bb2443f3909147f23395b4a1
meranhany25 <meranhany25@gmail.com>
2023-11-06 02:41:43 +0200

    k

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """ A function that prints integers from a list of lists """
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" ")

04402f6119a719fe07601afbc5d071e3bfc38d25
meranhany25 <meranhany25@gmail.com>
2023-11-06 16:16:44 +0200

    l'

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,37 @@
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
+ * is_palindrome - checks if a singly linked list is a palindrome
  *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
+ * @head: double pointer to the head of the linked list
  *
- * Return: 1 if it is, 0 if not
+ * Return: 1 if linked list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *current = *head;
+	int arr[1024];
+	int i = 0;
+	int j = 0;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	while (1)
+	if (*head == NULL)
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		return (0);
+	}
+	while (current != NULL)
+	{
+		arr[i++] = current->n;
+		current = current->next;
 	}
 
-	reverse_listint(&dup);
-
-	while (dup && temp)
+	for (j = 0; j < i / 2; j++)
 	{
-		if (temp->n == dup->n)
+		if (arr[j] != arr[i - 1 - j])
 		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
 			return (0);
+		}
+
 	}
+	return (1);
 
-	if (!dup)
-		return (1);
-
-	return (0);
 }

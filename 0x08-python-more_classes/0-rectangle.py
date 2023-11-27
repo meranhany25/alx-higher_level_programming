8b3ff8f329557ed4232b00e6ff8d9aa3a550161b
meranhany25 <meranhany25@gmail.com>
2023-11-27 20:37:24 +0200

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+Rectangle class.
+the module had an empty class
+use cases :
+new_rectangle = Rectangle()
+'''
+
+
+class Rectangle:
+    '''an empty class which name is rectangle'''
+    pass

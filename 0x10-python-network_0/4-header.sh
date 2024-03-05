040941dd4ea170d0e7c42d9ecd65ed5cd9794af2
meranhany25 <meranhany25@gmail.com>
2024-03-05 14:37:31 -0800

    m

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import requests
-
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+#!/bin/bash
+# script that takes in a URL as an argument and displays the body of the response
+curl -sH "X-School-User-Id: 98" "${1}"

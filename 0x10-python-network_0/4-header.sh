45f91c88e0c68ad7f2d903650a12974784f8ff3e
meranhany25 <meranhany25@gmail.com>
2024-03-05 14:27:12 -0800

    m

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-# script to send custom headers to servers
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+
+
+import requests
+response = requests.get('https://alx-intranet.hbtn.io/status')
+
+content = response.text
+print("Body response:")
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")

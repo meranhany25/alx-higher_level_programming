0031003e96c4b4108c70cbfb4e10e936c5ba530c
meranhany25 <meranhany25@gmail.com>
2024-03-05 14:30:13 -0800

    m

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-
-
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
-response = requests.get('https://alx-intranet.hbtn.io/status')
 
-content = response.text
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))

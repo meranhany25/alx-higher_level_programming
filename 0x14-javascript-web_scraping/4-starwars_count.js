4eef3e55d34a71ad38a8f363b5979b8bb96c33ba
meranhan25 <meranhany25@gmail.com>
2024-03-26 20:58:01 +0200

    rrr

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-
-request(url, (err, res, body) => {
-  let count = 0;
-
-  const cont = JSON.parse(body);
-  const results = cont.results;
-  results.forEach(elem => {
-    if (elem.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
+let count = 0;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body).results;
+    for (const film of json) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
     }
-  });
-  console.log(err || count);
-});
+    console.log(count);
+  }
+}
+);
